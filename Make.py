42bebce163a85d6b8994b0470ca11061ead21388
Russle <mfscales@gmail.com>
2017-01-05 12:55:57 -0600

    Status printing and command-line arg explanation added to Make.py

--- Make.py
+++ Make.py
@@ -3,7 +3,20 @@ pi = n.pi
 import os
 from sys import argv
 
-try:
+def println():
+    print('-----------------------------------------------------------')
+
+if len(argv) != 7:
+    println()
+    print('Requires 6 command-line arguments.')
+    print('[1] Experiment no. whose geometry and loading you\'re modeling.')
+    print('[2] Number of elements thru-thickness in the test section')
+    print('[3] Imperfection magnitude (as fraction, not percentage)')
+    print('[4] Eccentricity.  Give a number, or give "auto" and the geometry will match that of the test specimen.')
+    print('[5] Input file name')
+    print('[6] Constitutive model: "vm", "h8", or "anis"')
+    println()
+else:
     argv, expt, num_el_fine_th, dt, eccen, inpname, constit = argv
     d = n.genfromtxt('ExptParams.dat', delimiter=', ', dtype=str)
     # [0]Expt no, [1]IDg, [2]Mean thickness, [3]Min thickness
@@ -11,33 +24,41 @@ try:
     if eccen == 'auto':
         eccen = 1 - float(tmin)/float(tg)
         print('Eccen = {:.2f}'.format(eccen*100))
-except ValueError:
-    raise ValueError('Problem!')
-    expt = '20'
-    ID = '1.75' # Inner Radius
-    tg = '0.038'
-    num_el_fine_th = '12' # Num elements thru the test section thicknes
-    dt = '.01'
-    inpname = 'Input_20'
-    constit = 'vm'
-
-Lg = '0.2'
-Ltop = '1.3'  # Length of thick section above radius/chamf
-ODtop = '1.9685'    # Radius of thick section    
-R = '0.125'    # Radius of chamf
     
-### Nodes
-os.system('python Nodes.py {:s} {:s} {:s} {:s} {:s} {:s} {:s} {:s}'.format(
+    inpname = inpname.split('.')[0] # In case I give an extension.
+
+    Lg = '0.2'
+    Ltop = '1.3'  # Length of thick section above radius/chamf
+    ODtop = '1.9685'    # Radius of thick section    
+    R = '0.125'    # Radius of chamf
+    
+    ### Nodes
+    println()
+    print('Generating nodes.')
+    os.system('python Nodes.py {:s} {:s} {:s} {:s} {:s} {:s} {:s} {:s}'.format(
                 Lg, Ltop, ODtop, ID, tg, R, num_el_fine_th, dt))
+    print('Done.')
 
-### Elements
-os.system('python Elements.py')
+    ### Elements
+    println()
+    print('Generating elements.')
+    os.system('python Elements.py')
+    print('Done.')
 
-### Sets
-os.system('python Sets.py')
+    ### Sets
+    println()
+    print('Generating node and element sets.')
+    os.system('python Sets.py')
+    print('Done.')
 
-### Eccentricity
-os.system('python Eccen.py {}'.format(eccen))
+    ### Eccentricity
+    println()
+    print('Generating Eccentricity.')
+    os.system('python Eccen.py {}'.format(eccen))
+    print('Done.')
 
-### Write Input
-os.system('python WriteInput.py {:s} {:s} {:s}'.format(expt, inpname, constit))
+    ### Write Input
+    println()
+    print('Writing input file, {}.inp'.format(inpname))
+    os.system('python WriteInput.py {:s} {:s} {:s}'.format(expt, inpname, constit))
+    print('Done!')
